bc6c12af0245f9651cb7ada1d8a847a7fdd61c18
Mesut Tasci <mst.tasci@gmail.com>
2023-11-18 03:59:27 +0300

    put multiple keys in main examp

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ mod error;
 
 fn main() {
     let mut cask = FsStorage::load("/tmp/my_bitcask").unwrap();
-    if let Err(e) = cask.put(b"a1", b"a1 a2") {
-        panic!("{}", e)
-    }
+    cask.put(b"a1", b"a1 val");
+    cask.put(b"a2", b"a2 val");
+
     let key = "a1";
     if let Some(x) = cask.get(key.as_bytes()).unwrap() {
         println!("key found: `{:?}`", std::str::from_utf8(x.as_slice()));
